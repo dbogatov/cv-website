d50fcef3c31e860a61e1060ba1a3341baed1aeb8
Dima4ka <dbogatov@wpi.edu>
2016-08-25 12:14:27 -0400

    Add deploy script

---
+++ .deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# set -e
+
+cd "$(dirname "$0")"
+
+git fetch --all
+git reset --hard origin/master
+
+curl --request POST 'https://push.dbogatov.org/api/push/deploy' --data "project=CV-Website"
