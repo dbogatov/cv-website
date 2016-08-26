0844cec48e9a9a47e842f68cbbb4042af3cc3729
Dmytro <dbogatov@wpi.edu>
2016-08-25 21:00:16 -0400

    Fix.

--- .deploy.sh
+++ .deploy.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-#cd "$(dirname "$0")"
-
-# cd /var/static/CV-Website/
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
