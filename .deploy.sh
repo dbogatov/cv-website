478f93e5d382eea5023de2fb61d915ce29eeb9bc
Dmytro <dbogatov@wpi.edu>
2016-08-25 20:43:50 -0400

    Fix.

--- .deploy.sh
+++ .deploy.sh
@@ -4,10 +4,10 @@
 
 cd "$(dirname "$0")"
 
-git fetch --all
-git reset --hard origin/master
+git fetch --all >> tmp.txt 2>&1
+git reset --hard origin/master >> tmp.txt 2>&1
 
-npm install
-gulp
+npm install >> tmp.txt 2>&1
+gulp >> tmp.txt 2>&1
 
 curl --request POST 'https://push.dbogatov.org/api/push/deploy' --data "project=CV-Website"
