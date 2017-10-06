47d2ff6bfa60c870c9ccead09dce7266aa2afa20
Dmytro Bogatov <dmytro@dbogatov.org>
2017-10-06 00:15:22 -0400

    Get resume in CI.

---
+++ put-resume.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_ID="30" # lookup in repo settings
+JOB="artifacts" # change if necessary
+BRANCH="master"
+
+echo "Downloading artifacts into temporary directory"
+cd `mktemp -d`
+curl \
+	--header "PRIVATE-TOKEN: $TOKEN" \
+	"https://git.dbogatov.org/api/v4/projects/$PROJECT_ID/jobs/artifacts/$BRANCH/download?job=$JOB" \
+> artifacts.zip
+
+echo "Extracting files"
+unzip artifacts.zip
+
+echo "Moving file"
+mv resume.pdf wwwroot/assets/custom/docs/resume.pdf
+
+echo "Done"
