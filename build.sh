5cc51fc9718b2bfe323560b025c50962764df40b
Dmytro Bogatov <dmytro@dbogatov.org>
2017-11-16 20:01:06 -0500

    Fix.
    Former-commit-id: 87831fe58be51aafbf59b6f7b085a802f16ea99b

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Cleaning output directory..."
 rm -f wwwroot/*.html
 
 echo "Installing node modules... Requires NPM"
-npm install --loglevel=error > /dev/null
+npm install --unsafe-perm --loglevel=error
 
 echo "Generating HTML... Requires Gulp (installed by NPM)"
 $(npm bin)/gulp > /dev/null
